cdad3574db4be65463db586e0cc0875e06d89c0d
scottinet <scottinet@kuzzle.io>
2017-06-02 07:33:19 +0200

    add more unit tests

--- test/deepclone.test.js
+++ test/deepclone.test.js
@@ -1,5 +1,6 @@
 const 
   assert = require('assert'),
+  crypto = require('crypto'),
   deepclone = require('..');
 
 describe('deepclone unit tests', () => {
@@ -10,7 +11,8 @@ describe('deepclone unit tests', () => {
         baz: {
           qux: 'qux'
         }
-      }
+      },
+      qux: [1, 'foo', 3.14, {bar: 'baz'}]
     };
 
     const target = deepclone(src);
@@ -35,5 +37,240 @@ describe('deepclone unit tests', () => {
     assert.deepEqual(src, target);
     assert.strictEqual(target.bar.baz, target);
     assert.notStrictEqual(target.bar.baz, src);
+  });
+
+  it('should return the provided argument if it is not an object', () => {
+    for (const arg of [undefined, null, 3.14, 123, 'foobar']) {
+      assert.strictEqual(arg, deepclone(arg));
+    }
+  });
+
+  it('clone/copy: Map', () => {
+    const src = {
+      foo: 'bar',
+      bar: 123,
+      baz: new Map([[1, 2], [2, 3], [3, 4]])
+    };
+
+    const cloned = deepclone(src);
+
+    assert.deepEqual(src, cloned);
+    assert.strictEqual(src.baz, cloned.baz);
+
+    const copied = deepclone(src, true);
+
+    assert.deepEqual(src, copied);
+    assert.notStrictEqual(src.baz, copied.baz);
+    assert.deepEqual([...src.baz.entries()], [...copied.baz.entries()]);
+
+  });
+
+  it('clone/copy: Set', () => {
+    const src = {
+      foo: 'bar',
+      bar: 123,
+      baz: new Set([1, 2, 3, 4, 5])
+    };
+
+    const cloned = deepclone(src);
+
+    assert.deepEqual(src, cloned);
+    assert.strictEqual(src.baz, cloned.baz);
+
+    const copied = deepclone(src, true);
+
+    assert.deepEqual(src, copied);
+    assert.notStrictEqual(src.baz, copied.baz);
+    assert.deepEqual([...src.baz.entries()], [...copied.baz.entries()]);
+  });
+
+  it('clone/copy: Buffer', () => {
+    const src = {
+      foo: 'bar',
+      bar: 123,
+      baz: crypto.randomBytes(12)
+    };
+
+    const cloned = deepclone(src);
+
+    assert.deepEqual(src, cloned);
+    assert.strictEqual(src.baz, cloned.baz);
+
+    const copied = deepclone(src, true);
+
+    assert.deepEqual(src, copied);
+    assert.notStrictEqual(src.baz, copied.baz);
+    assert.deepEqual([...src.baz.entries()], [...copied.baz.entries()]);
+  });
+
+  it('clone/copy: Float64Array', () => {
+    const src = {
+      foo: 'bar',
+      bar: 123,
+      baz: new Float64Array(crypto.randomBytes(12))
+    };
+
+    const cloned = deepclone(src);
+
+    assert.deepEqual(src, cloned);
+    assert.strictEqual(src.baz, cloned.baz);
+
+    const copied = deepclone(src, true);
+
+    assert.deepEqual(src, copied);
+    assert.notStrictEqual(src.baz, copied.baz);
+    assert.deepEqual([...src.baz.entries()], [...copied.baz.entries()]);
+  });
+
+  it('clone/copy: Float32Array', () => {
+    const src = {
+      foo: 'bar',
+      bar: 123,
+      baz: new Float32Array(crypto.randomBytes(12))
+    };
+
+    const cloned = deepclone(src);
+
+    assert.deepEqual(src, cloned);
+    assert.strictEqual(src.baz, cloned.baz);
+
+    const copied = deepclone(src, true);
+
+    assert.deepEqual(src, copied);
+    assert.notStrictEqual(src.baz, copied.baz);
+    assert.deepEqual([...src.baz.entries()], [...copied.baz.entries()]);
+  });
+
+  it('clone/copy: Int32Array', () => {
+    const src = {
+      foo: 'bar',
+      bar: 123,
+      baz: new Int32Array(crypto.randomBytes(12))
+    };
+
+    const cloned = deepclone(src);
+
+    assert.deepEqual(src, cloned);
+    assert.strictEqual(src.baz, cloned.baz);
+
+    const copied = deepclone(src, true);
+
+    assert.deepEqual(src, copied);
+    assert.notStrictEqual(src.baz, copied.baz);
+    assert.deepEqual([...src.baz.entries()], [...copied.baz.entries()]);
+  });
+
+  it('clone/copy: Int16Array', () => {
+    const src = {
+      foo: 'bar',
+      bar: 123,
+      baz: new Int16Array(crypto.randomBytes(12))
+    };
+
+    const cloned = deepclone(src);
+
+    assert.deepEqual(src, cloned);
+    assert.strictEqual(src.baz, cloned.baz);
+
+    const copied = deepclone(src, true);
+
+    assert.deepEqual(src, copied);
+    assert.notStrictEqual(src.baz, copied.baz);
+    assert.deepEqual([...src.baz.entries()], [...copied.baz.entries()]);
+  });
+
+  it('clone/copy: Int8Array', () => {
+    const src = {
+      foo: 'bar',
+      bar: 123,
+      baz: new Int8Array(crypto.randomBytes(12))
+    };
+
+    const cloned = deepclone(src);
+
+    assert.deepEqual(src, cloned);
+    assert.strictEqual(src.baz, cloned.baz);
+
+    const copied = deepclone(src, true);
+
+    assert.deepEqual(src, copied);
+    assert.notStrictEqual(src.baz, copied.baz);
+    assert.deepEqual([...src.baz.entries()], [...copied.baz.entries()]);
+  });
+
+  it('clone/copy: Uint8Array', () => {
+    const src = {
+      foo: 'bar',
+      bar: 123,
+      baz: new Uint8Array(crypto.randomBytes(12))
+    };
+
+    const cloned = deepclone(src);
+
+    assert.deepEqual(src, cloned);
+    assert.strictEqual(src.baz, cloned.baz);
+
+    const copied = deepclone(src, true);
+
+    assert.deepEqual(src, copied);
+    assert.notStrictEqual(src.baz, copied.baz);
+    assert.deepEqual([...src.baz.entries()], [...copied.baz.entries()]);
+  });
+
+  it('clone/copy: Uint8ClampedArray', () => {
+    const src = {
+      foo: 'bar',
+      bar: 123,
+      baz: new Uint8ClampedArray(crypto.randomBytes(12))
+    };
+
+    const cloned = deepclone(src);
+
+    assert.deepEqual(src, cloned);
+    assert.strictEqual(src.baz, cloned.baz);
+
+    const copied = deepclone(src, true);
+
+    assert.deepEqual(src, copied);
+    assert.notStrictEqual(src.baz, copied.baz);
+    assert.deepEqual([...src.baz.entries()], [...copied.baz.entries()]);
+  });
+
+  it('clone/copy: Date', () => {
+    const src = {
+      foo: 'bar',
+      bar: 123,
+      baz: new Date()
+    };
+
+    const cloned = deepclone(src);
+
+    assert.deepEqual(src, cloned);
+    assert.strictEqual(src.baz, cloned.baz);
+
+    const copied = deepclone(src, true);
+
+    assert.deepEqual(src, copied);
+    assert.notStrictEqual(src.baz, copied.baz);
+    assert.deepEqual(src.baz.valueOf(), copied.baz.valueOf());
+  });
+
+  it('clone/copy: RegExp', () => {
+    const src = {
+      foo: 'bar',
+      bar: 123,
+      baz: /^foobar/ig
+    };
+
+    const cloned = deepclone(src);
+
+    assert.deepEqual(src, cloned);
+    assert.strictEqual(src.baz, cloned.baz);
+
+    const copied = deepclone(src, true);
+
+    assert.deepEqual(src, copied);
+    assert.notStrictEqual(src.baz, copied.baz);
+    assert.deepEqual(src.baz.toString(), copied.baz.toString());
   });
 });
